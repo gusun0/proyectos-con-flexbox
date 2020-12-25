a391ec788ae6539e7eac0a38555fdba8ddc8d5d6
gusun0 <fpatan@mail.ru>
2020-12-25 02:30:25 -0600

    flex

--- airbnb-inicio/js/scripts.js
+++ airbnb-inicio/js/scripts.js
@@ -1,6 +1,39 @@
-// Btn flotante en el footer
-
 document.addEventListener('DOMContentLoaded', () => {
+
+    /** Obtiene el hero con las imagenes */
+    const imagenHero = document.querySelector('.hero');
+    console.log(imagenHero);
+
+    /** Arreglo de imagenes con fondo */
+    const imagenes = ['arriba2.jpg', 'arriba.jpg' ];
+
+    /**Inicializadores */
+    let i = 0;
+    let tiempo = 0;
+    setInterval( () => {
+        imagenHero.style.backgroundPositionY =  '-'+tiempo+'px';
+        // console.log(tiempo);
+        if(tiempo > 40) {
+            tiempo = 0;
+            imagenHero.style.backgroundImage = "url(../img/"+imagenes[i]+")";
+            
+            if(i === imagenes.length - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+        
+        tiempo += 1;
+    }, 100);
+});
+
+
+
+
+
+
+
  const btnFlotante = document.querySelector('.btn-flotante');
  btnFlotante.addEventListener('click', (e) => {
 	 e.preventDefault();
@@ -20,4 +53,3 @@ document.addEventListener('DOMContentLoaded', () => {
 		btnFlotante.innerText = 'Cerrar';
 	}
  });
-});
